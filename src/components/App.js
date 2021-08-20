c0abd1f561c7f75699bc45efd5516c11ab613284
Obetta <francischassobetta@gmail.com>
2021-08-20 20:54:52 +0100

    Style App component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,13 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
+const styles = {
+  container: {
+    width: '700px',
+    margin: 'auto',
+  },
+};
+
 export default function App() {
   const [total, setTotal] = useState(null);
   const [next, setNext] = useState(null);
@@ -16,9 +23,9 @@ export default function App() {
   };
 
   return (
-    <>
+    <div style={styles.container}>
       <Display result={next || total || '0'} />
       <ButtonPanel clickHandler={handleClick} />
-    </>
+    </div>
   );
 }
