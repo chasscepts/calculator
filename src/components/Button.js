a1da723b4b7abcc255e2c47379ad9cdc913c65a0
Obetta <francischassobetta@gmail.com>
2021-08-20 17:06:14 +0100

    Attach clickHandler to button onClick event

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function Button({ name }) {
+function Button({ name, clickHandler }) {
+  function handleClick() {
+    clickHandler(name);
+  }
+
   return (
-    <button type="button"><span>{name}</span></button>
+    <button type="button" onClick={handleClick}><span>{name}</span></button>
   );
 }
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default Button;
