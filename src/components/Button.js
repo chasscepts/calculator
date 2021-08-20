415ac54181cc187a8cbfb1dc54f70a5f5e2207f5
Obetta <francischassobetta@gmail.com>
2021-08-20 20:53:12 +0100

    Add button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,38 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function Button({ name, clickHandler }) {
+const buttonStyles = (color, wide) => ({
+  flex: wide ? '0 0 50%' : '0 0 25%',
+  backgroundColor: color,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: '#000',
+  border: '2px solid gray',
+  fontWeight: 'bold',
+  fontSize: '24px',
+});
+
+export default function Button({
+  name, color, wide, clickHandler,
+}) {
   function handleClick() {
     clickHandler(name);
   }
 
   return (
-    <button type="button" onClick={handleClick}><span>{name}</span></button>
+    <button type="button" style={buttonStyles(color, wide)} onClick={handleClick}><span>{name}</span></button>
   );
 }
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
   clickHandler: PropTypes.func.isRequired,
 };
 
-export default Button;
+Button.defaultProps = {
+  color: '#ddd',
+  wide: false,
+};
