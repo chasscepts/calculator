d9a929225942cdfa3b2113e2cde2de29364524f2
Obetta <francischassobetta@gmail.com>
2021-08-21 06:07:17 +0100

    Add quote of the day page

---
+++ src/components/Quote.js
@@ -0,0 +1,7 @@
+const quote = 'Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. – William Paul Thurston';
+
+export default function Quote() {
+  return (
+    <p className="content">{quote}</p>
+  );
+}
